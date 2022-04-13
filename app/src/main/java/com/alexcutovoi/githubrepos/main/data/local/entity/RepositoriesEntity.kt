019e16dcf40efe6961684439c129c314d1d59373
Alex Cutovoi <alexcutovoi@gmail.com>
2022-04-13 07:55:33 -0300

    Remove repositories entity

--- app/src/main/java/com/alexcutovoi/githubrepos/main/data/local/entity/RepositoriesEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.alexcutovoi.githubrepos.main.data.local.entity
-
-import androidx.room.Entity
-
-//@Entity(tableName = "repositories")
-data class RepositoriesEntity (
-    val repositoryList: List<RepositoryEntity>,
-    val totalCount: Int
-)
